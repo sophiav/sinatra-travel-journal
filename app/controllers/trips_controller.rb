f8ea0dcf56fd158f0a83134753cdd88a1b85f813
Sophia Varella <sophiavarella@gmail.com>
2017-05-26 12:48:32 -0700

    Implement new trip functionality: create new trip form, controller and update trips index form with a link to the 'New Trip' page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,5 +3,21 @@ class TripsController < ApplicationController
     erb :'/trips/index'
   end
 
+  get '/trips/new' do # create trip
+    erb :'/trips/new'
+  end
+
+  post '/trips' do
+    puts params
+    @trip = Trip.new(name: params[:name])
+    @trip.user_id = session[:user_id]
+
+    if @trip.save
+      redirect "/trips/#{@trip.id}"
+    else
+      redirect '/trips/new'
+    end
+  end
+
 end
 
